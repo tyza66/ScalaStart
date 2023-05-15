dd396c9f1df8fe764be027ab6768f45f05533710
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-15 09:10:58 +0800

    update

---
+++ 代码/ScalaDemo/src/main/scala/c1_HelloWorld/HelloScala.scala
@@ -0,0 +1,20 @@
+package c1_HelloWorld
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/15 8:58
+ * Github: https://github.com/tyza66
+ **/
+object HelloScala { //object中所有的属性和方法默认都是静态的
+  //scala中没有静态的关键字 使用伴生对象表示静态的属性和方法
+  def main(args: Array[String]): Unit = {
+    System.out.println("Hello,World!")
+  }
+}
+
+//如果写一个同名类 这个类叫做伴生类 这俩加起来算一个java里面的类
+//换句话说object里面的都是静态的 class里面不是静态的
+//如果单独存在一个的话 这个对象就叫单例对象 如果都有就叫伴生对象
+class HelloScala{
+
+}
