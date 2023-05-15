24c962de631b91eab2d4ea2f35e25298fa7ba498
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-15 12:57:10 +0800

    update

---
+++ 代码/ScalaDemo/src/main/scala/c2_zhushi/Zhushi.scala
@@ -0,0 +1,20 @@
+package c2_zhushi
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/15 11:23
+ * Github: https://github.com/tyza66
+ **/
+object Zhushi {
+  def main(args: Array[String]): Unit = {
+    //单行注释
+
+    /*
+      多行注释
+    */
+
+    /*
+    * 文档注释 在上面添加文档注释的时候会自动添加相关的参数属性
+    * */
+  }
+}
---
+++ 代码/ScalaDemo/src/main/scala/c3_var_val/VarAndVal.scala
@@ -0,0 +1,30 @@
+package c3_var_val
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/15 12:03
+ * Github: https://github.com/tyza66
+ **/
+object VarAndVal {
+  def main(args: Array[String]): Unit = {
+    val v : Int = 10 //常量值不能修改
+    var v2 : Int = 20 //变量的值可以修改
+    var v3 = 10 //c3有类型 根据类型推断而来 变量类型一旦定义就不可以被修改
+    //类型推断只能推断固定的类型 比如自动推断只会推断成int不会推断成byte
+    //变量声明时必须要写初始值
+
+    //定义一个新的对象 查看默认值是多少
+    val person2 = new Person2()
+    println(person2.age)
+    //常量的引用数据类型 引用是不能被修改的
+    val person3 = new Person2
+    //person3 = new Person2
+    //但是引用数据类型常量的非常量属性是可以被改变的
+    person3.age = 1;
+  }
+}
+
+class Person2{
+  //可以使用——表示初始值 但是常量不能用_因为常量的值不能改变了 这样也会获得一个初始值
+  var age:Int = _;
+}
