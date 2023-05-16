e96d783e520b594754d3cc12099dfa9d3302ded5
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-16 09:31:13 +0800

    update

---
+++ 代码/ScalaDemo/src/main/scala/c8_leixing/TypeChange.scala
@@ -0,0 +1,12 @@
+package c8_leixing
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/16 9:27
+ * Github: https://github.com/tyza66
+ **/
+object TypeChange {
+  def main(args: Array[String]): Unit = {
+    //scala中的类型转换也是按照精度/容量大小进行转换的
+  }
+}
