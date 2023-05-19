dcf7629c7a4c8c3a512844d5401a32c057c9438c
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-20 06:39:20 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Collection9.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection9.scala
@@ -12,7 +12,17 @@ object Collection9 {
     val list = List(1, 2, 3, 4, 5)
     //参数是一个方法 返回值为真就保留假就不要
     println(list.filter(_ == 2))
-    //转换结构
-    
+    //转化、映射 转换结构 转换成的结构根据返回的值的类型
+    list.map((i: Int) => i * 2)
+
+    //扁平化 就是集合中嵌套集合的时候我们要给里面的集合分开
+    val list2:List[List[Int]] = List(List(1,2,3,4),List(3,4,5,6))
+    val list3:List[Int] = list2.flatten //元素必须是可以拆分的集合才能调用扁平化
+    println(list3)
+    //字符串可以看作char的数组
+    val strings: List[String] = List("giao giao", "giao")
+    println(strings.flatten)
+
+    //分组
   }
 }
