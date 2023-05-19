a63dd1d404e22ae7d8128db5988539a507c37a8a
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-19 15:14:13 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Collection7.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection7.scala
@@ -7,6 +7,16 @@ package c13_collection
  **/
 object Collection7 {
   def main(args: Array[String]): Unit = {
+    //集合的衍生
+    val list = List(1, 2, 3, 4, 5)
+    //获取集合的头
+    println(list.head)
+    //获取集合的尾
+    //集合最后一个数据
+    //集合初始数据
+    //反转
+    //取前n个元素
+    //去掉前n个元素
 
   }
 }
