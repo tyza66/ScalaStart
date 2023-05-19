2b27ed4bd1a6dbd5119727b738a4fc538ca7d41c
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-20 07:02:18 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Collection9.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection9.scala
@@ -16,13 +16,31 @@ object Collection9 {
     list.map((i: Int) => i * 2)
 
     //扁平化 就是集合中嵌套集合的时候我们要给里面的集合分开
-    val list2:List[List[Int]] = List(List(1,2,3,4),List(3,4,5,6))
-    val list3:List[Int] = list2.flatten //元素必须是可以拆分的集合才能调用扁平化
+    val list2: List[List[Int]] = List(List(1, 2, 3, 4), List(3, 4, 5, 6))
+    val list3: List[Int] = list2.flatten //元素必须是可以拆分的集合才能调用扁平化
     println(list3)
     //字符串可以看作char的数组
     val strings: List[String] = List("giao giao", "giao")
     println(strings.flatten)
 
     //分组
+    val tuples = List(("a", 10), ("b", 11), ("c", 12), ("d", 13), ("a", 100), ("b", 110), ("c", 120), ("d", 130))
+    val stringToTuples: Map[String, List[(String, Int)]] = tuples.groupBy((tuple: (String, Int)) => tuple._1)
+    println(stringToTuples)
+
+    //规约 合并 reduce返回结果类型和当前类型一致
+    val ints: List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    //第一个元素作为第一次规约调用的初始值
+    println(ints.reduce((res: Int, elem: Int) => (res + elem * 2)))
+
+    //折叠 fold 可以指定初始值
+    println(ints.fold(0)((res: Int, elem: Int) => (res + elem * 2)))
+
+    //改变数据类型
+    val tuple: (String, Int) = ints.foldLeft(("sum:elem*2", 0))((res: (String, Int), elem: Int) => {
+      (res._1, res._2 + elem *2)
+    })
+    println(tuple)
+    //reduceRight是个坑 不要用
   }
 }
