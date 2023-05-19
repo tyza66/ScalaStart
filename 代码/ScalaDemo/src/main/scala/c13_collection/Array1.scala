63adcf2a65062b91be2539de877246a659f425bb
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-19 09:38:22 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Array1.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Array1.scala
@@ -36,7 +36,44 @@ object Array1 {
 
     //可变长度数组 里面参数的大小只是底层结构的长度大小 默认是16
     val buffer:ArrayBuffer[Int] = new ArrayBuffer[Int](2)
-    buffer.append(1)
+    buffer.append(1,2,3) //添加元素 这里可以输入任意数量的参数
     println(buffer.length)
+
+    buffer.remove(0,2) //删除元素 有两个参数 第一个是从哪个下标开始 第二个是删除的个数
+    println(buffer.length)
+
+    buffer.update(0,96) //修改元素 两个参数 第一个是修改的下标 第二个是修改成的值 其实可以直接调用伴生类的apply方法
+
+    //往数组中添加元素 可变的用方法 不可变的用符号 这样不容易混
+    var array3:Array[Int] = array2 :+ 1;
+    println(array3.toList)
+
+    var buffer3:ArrayBuffer[Int] = buffer :+ 2;
+    println(buffer3)
+    buffer += 1
+    println(buffer.toList)
+
+    //可变数组和不可变数组的相互转换 直接使用to方法就可以
+
+    //定义二位数组
+    val array1: Array[Array[Int]] = Array(Array(0, 1), Array(1, 2), Array(2, 3))
+    for (elem <- array1) {
+      for (elem <- elem) {
+        print(elem + "\t")
+      }
+      println()
+    }
+
+    //scala中提供了生成多维数组的方法
+    val array4: Array[Array[Int]] = Array.ofDim[Int](3, 4) //表示产生一个三行四列的二维数组
+    //里面的值默认都是0
+    array4(0)(0) = 96
+    for (elem <- array4) {
+      for (elem <- elem) {
+        print(elem + "\t")
+      }
+      println()
+    }
+
   }
 }
---
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection2.scala
@@ -0,0 +1,32 @@
+package c13_collection
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/19 8:34
+ * Github: https://github.com/tyza66
+ **/
+object Collection2 {
+  def main(args: Array[String]): Unit = {
+    //不可变集合List 使用伴生类对象的apply方法
+    //scala中的不可变集合中的数和长度都是不可改变的
+    val ints = List(1, 2, 3, 4)
+    ints.foreach(println)
+    //添加元素
+    var ints2: List[Int] = ints :+ 96
+    ints2.foreach(println)
+
+    //使用头插法添加元素
+    var ints3: List[Int] = 96 :: ints2
+    ints3.foreach(println)
+
+    //集合之间合并
+    var ints4: List[Int] = ints3 ::: ints2
+
+    //空集合
+    val nil: Nil.type = Nil
+    val list96 = 1 :: 2 :: 3 :: Nil
+    val list97 = 1 :: 2 :: 3 :: List()
+
+    //可变集合ListBuffer
+  }
+}
