420d4e51dbbb96923d3f5f99c51d7aaf63adb980
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-19 19:29:50 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Collection8.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection8.scala
@@ -7,6 +7,19 @@ package c13_collection
  **/
 object Collection8 {
   def main(args: Array[String]): Unit = {
+    val list = List(1, 2, 3, 4, 6, 7, 8,-1)
     //集合计算的初级函数
+    //求和 sum
+    //乘积 product
+    //最大值 max
+    //最小值 min
+    //排序 sorted 默认使用的是快速排序 可以填入一个参数表示排序的顺序
+    println(list.sorted(Ordering.Int.reverse))
+    println(list.sorted(Ordering[Int].reverse))
+    //自定义排序sortBy 自定义排序规则 参数是一个函数 规则中可以是二元组 要以谁排序只要将要依据返回就行了 要倒转就要用柯里化的写法
+
+    //sortWith 参数是两个二元组为参数的函数 相当于满足要求就调换位置
+
+    //这些作为参数的函数可以简写成_
   }
 }
---
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection9.scala
@@ -0,0 +1,14 @@
+package c13_collection
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/19 19:22
+ * Github: https://github.com/tyza66
+ **/
+object Collection9 {
+  def main(args: Array[String]): Unit = {
+    //集合的高阶函数
+    //过滤
+    val list = List(1, 2, 3, 4, 5)
+  }
+}
