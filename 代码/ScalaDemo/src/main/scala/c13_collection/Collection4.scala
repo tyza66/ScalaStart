18432da27c127683c9c20d41c4e9a0893710c861
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-19 11:23:27 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Collection4.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection4.scala
@@ -1,5 +1,7 @@
 package c13_collection
 
+import scala.collection.mutable
+
 /**
  * Author: tyza66
  * Date: 2023/05/19 10:42
@@ -20,14 +22,27 @@ object Collection4 {
       println(s"$key => $value")
     }
     //基本数据类型 表示不存在 存在返回Some 不存在返回None
-    val maybeInt: Option[Int] = map2.get(3)
+    val maybeInt: Option[Int] = map2.get(2)
     println(maybeInt)
     //获得值不用强转 直接get
     maybeInt.get
     //这样调用就会出现问题 如果没货得到就不能get
-    val value1:Int = map.get("hello1").get
+    val value1:Int = map.get("hello").get
     maybeInt.getOrElse(10) //传入一个名调用 如果没有值的话就用这个值
     //上面那个太长了 我们可以直接在map中拿值的时候使用这个方法
-    val i: Int = map.getOrElse("hello", 1)
+    val i: Int = map.getOrElse("hello1", 1) //也是不存在就返回默认值
+
+    //可变map
+    val stringToInt: mutable.Map[String, Int] = mutable.Map(("giao", 1), ("giao2", 2))
+    //增加的时候 如果原来的键的位置有值的话 就会覆盖
+    val maybeInt1: Option[Int] = stringToInt.put("giao", 2)
+    println(maybeInt1)
+    println(stringToInt)
+    //删除
+    val maybeInt2: Option[Int] = stringToInt.remove("giao2")
+    println(maybeInt2)
+    println(stringToInt)
+    //修改整的活和put是一样的
+    stringToInt.update("giao", 3)
   }
 }
