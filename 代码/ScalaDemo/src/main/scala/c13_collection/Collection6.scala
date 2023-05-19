e75a829c7328b83094c222f0d9e6b31741f0a2e0
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-19 12:17:22 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c13_collection/Collection6.scala
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection6.scala
@@ -15,9 +15,15 @@ object Collection6 {
     //获得集合大小
     val size: Int = list.size
     println(size) //长度的大小都是获得集合里面元素的个数
-    //循环遍历
-    //迭代器
-    //生成字符串
+    //循环遍历 foreach
+    //迭代器 iterator
+    //生成字符串 tostring
+    val string: String = list.mkString //直接是元素
+    println(string)
+    val string2: String = list.mkString("\t") //可以传递参数作为间隔 但是不包括头尾
+    println(string2)
+    val string3: String = list.mkString("A","\t","B") //可以传递三个参数 开头 分隔 结尾
+    println(string3)
     //是否包含
   }
 }
---
+++ 代码/ScalaDemo/src/main/scala/c13_collection/Collection7.scala
@@ -0,0 +1,12 @@
+package c13_collection
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/19 12:11
+ * Github: https://github.com/tyza66
+ **/
+object Collection7 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
