0ba1756d7016b0038efe5f08c91e2033e869b47a
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-20 19:05:03 +0800

    update

--- 代码/ScalaDemo/src/main/scala/c16_yinshi/Yinshi.scala
+++ 代码/ScalaDemo/src/main/scala/c16_yinshi/Yinshi.scala
@@ -7,6 +7,20 @@ package c16_yinshi
  **/
 object Yinshi {
   def main(args: Array[String]): Unit = {
-    
+    //当编译器第一次编译失败的时候，会在当前的环境中查找能让代码编译通过的方法，用于将类型进行隐式转换，实现第二次编译
+    //隐式函数
+    //将当前作用域下的所有传入值的类型隐式转换成返回值的类型
+    implicit def changeInt(self:Int) = {
+      new MyInt(self)
+    }
+    val i1 = 10
+    val i:Int = i1.myMax(20)
+    println(i)
+  }
+}
+
+class MyInt(val self:Int){
+  def myMax(i:Int):Int = {
+    if(i > self) i else self
   }
 }
